bcce0c6d17583ff3e6a5ff4301c489e986b81863
tosucity <tnkdisk@gmail.com>
2013-08-04 18:19:42 +0900

    ICON SET

--- js/setting.js
+++ js/setting.js
@@ -18,21 +18,21 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* tosu cofog */
-  '101': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '102': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '103': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
-  '104': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
-  '105': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  '106': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '107': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '108': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '109': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '110': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
-//  '111': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
-//  '112': { icon: 'icons/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
-//  '113': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#D33673' },
-//  '114': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
-//  '115': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
+  '101': { icon: 'icons/tosu/101civilian-action.svg', color: '#C75746', bcolor: '#935B3B' },
+  '102': { icon: 'icons/tosu/102government-uk.svg', color: '#C75746', bcolor: '#935B3B' },
+  '103': { icon: 'icons/tosu/103family.svg', color: '#C75746', bcolor: '#0AB971' },
+  '104': { icon: 'icons/tosu/104environment.svg', color: '#C75746', bcolor: '#0AB971' },
+  '105': { icon: 'icons/tosu/105labour.svg', color: '#C75746', bcolor: '#0AB971' },
+  '106': { icon: 'icons/tosu/106farms.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '107': { icon: 'icons/tosu/107c_syoukougyou.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '108': { icon: 'icons/tosu/108construction.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '109': { icon: 'icons/tosu/109fire-brigade.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '110': { icon: 'icons/tosu/110schools.svg', color: '#C75746', bcolor: '#D33673' },
+//  '111': { icon: 'icons/tosu/farms.svg', color: '#C75746', bcolor: '#D33673' },
+//  '112': { icon: 'icons/tosu/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
+//  '113': { icon: 'icons/tosu/housing.svg', color: '#C75746', bcolor: '#D33673' },
+//  '114': { icon: 'icons/tosu/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
+//  '115': { icon: 'icons/tosu/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
 };
 
 
